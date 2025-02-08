2289d882c0f7a6bf787394526e9cea91e06b1f47
Tsehay21 <tsehay1900@yahoo.com>
2025-02-08 07:36:52 -0500

    Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import React from "react";
 
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
