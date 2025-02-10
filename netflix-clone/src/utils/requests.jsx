6103f0855a830aa0b1db63f94b6a1423c0f5dea0
Tsehay21 <tsehay1900@yahoo.com>
2025-02-10 00:17:37 -0500

    sign

--- netflix-clone/src/utils/requests.jsx
+++ netflix-clone/src/utils/requests.jsx
@@ -6,7 +6,7 @@ const requests = {
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_generes=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_generes=35`,
-  fetchHorrorMovies: `/discover/movie>api_key=${API_KEY}&with_genres=27`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
   fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
