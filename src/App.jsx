280113fe010720e4b57818a2fce522e90acb4e06
Tsehay21 <tsehay1900@yahoo.com>
2025-02-10 20:47:15 -0500

    Api_Key

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import React from "react";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home />
     </div>
   );
